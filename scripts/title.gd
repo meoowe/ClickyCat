47f22b9307e29b05541eea374913e10b2801ea2a
meoowe <96916064+meoowe@users.noreply.github.com>
2024-12-07 23:30:29 +0000

    complete leader board functionality

--- scripts/title.gd
+++ scripts/title.gd
@@ -14,3 +14,7 @@ func _on_credits_pressed() -> void:
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_texture_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://leaderboard.tscn")
