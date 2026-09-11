81f8a4c315c78f2a6a4cc4870751a7258c155276
Aarav Kataria <meoowe990@gmail.com>
2026-09-11 23:43:49 +0100

    Delete useless files, rework save system, modify Lose to use parallax bg

--- scripts/autoloads/Global.gd
+++ scripts/autoloads/Global.gd
@@ -9,7 +9,7 @@ var base_log: LoggieMsg = Loggie.msg("[Global]").bold().color(Color.CORNFLOWER_B
 @export var playerName: String = "Nickname"
 @export var LEADERBOARD_ID: String = "clicky-cat-clickycat-njjy" #TODO: Refactor this to be lowercase
 @export var VERSION: String = "READ-THE-NOTE-SILLY" # NOTE: Do NOT Edit this variabe. Edit through global.tscn instead.!
-@export var save_file_path: String = ""
+@export var save_file_path: String = "user://clickycat.tres"
 
 @export_category("Game Settings")
 @export var scoreIncrement: int = 10
@@ -22,6 +22,9 @@ var balloonClicked: bool = false
 var platform: String = ""
 var new_high_score: bool = false
 var game_has_started: bool = false
+var cheats_used: bool = false
+
+signal save_discarded
 
 var platforms: Dictionary[Variant, Variant] = {
 	"web": "web",
@@ -40,7 +43,9 @@ func _ready() -> void:
 	Loggie.msg("[Global]").bold().color(Color.CORNFLOWER_BLUE).add(" Version: ", VERSION).info()
 	determine_platform()
 	title_music.play()
+	Loggie.msg("[Global]").bold().color(Color.CORNFLOWER_BLUE).add(" Load_from_save()").debug()
 	load_from_save()
+	print(OS.get_user_data_dir())
 
 
 func determine_platform() -> String:
@@ -66,30 +71,50 @@ func wait(time: float):
 	await get_tree().create_timer(time).timeout
 	
 func save():
+	print("Save() was called!")
 	var data = SaveData.new()
 	data.config_version = VERSION
 	data.name = playerName
 	data.high_score = highScore
-	
+	data.cheats_used = cheats_used
+	print("got to save call")
 	var error = ResourceSaver.save(data, save_file_path)
-	if error:
-		base_log.add(" Failed  to save! ", error).error()
+	if error != OK:
+		Loggie.msg("[Global]").bold().color(Color.CORNFLOWER_BLUE).add(" Failed  to save! ", error).error()
+		return
+	Loggie.msg("[Global]").bold().color(Color.CORNFLOWER_BLUE).add(" Saved game!").info()
+	print("saved game")
 
 func load_from_save():
+	print("load_from_save() was called")
 	if !ResourceLoader.exists(save_file_path):
 		base_log.add(" No save file exists! Skipping load!").warn()
 		return
 	var data: SaveData = load(save_file_path)
+	Loggie.msg("[Global]").bold().color(Color.CORNFLOWER_BLUE).add(
+		" Found save file. Loading data:"
+		).hseparator().add(
+			"High Score: ", data.high_score
+			).nl().add(
+				"Name: ", data.name
+				).nl().add(
+					"Cheats:", data.cheats_used
+				).box().info()
 	if data.config_version != VERSION:
 		base_log.add(" Tried to load diffrent config ver save! Aborting!").error()
-	data.high_score = highScore
-	data.name = playerName
+		save_discarded.emit()
+		return
+	if data.cheats_used:
+		base_log.add(" Previous log used cheats, tut tut tut").info()
+	highScore = data.high_score
+	playerName = data.name
 
 func _notification(what: int) -> void:
 	if what == NOTIFICATION_WM_CLOSE_REQUEST:
 		Loggie.msg("[Global]").bold().color(Color.CORNFLOWER_BLUE).add("🚪 Recieved WM Close Request, saving and gracefully quitting.").info()
-		save()
+		print("_notification: calling save!")
 		get_viewport().set_input_as_handled()
+		save()
 		if OS.has_feature("web"):
 			JavaScriptBridge.eval("window.close()")
 		else:
--- scripts/game_ui.gd
+++ scripts/game_ui.gd
@@ -1,6 +1,24 @@
 extends CanvasLayer
 
 
+var active_tween: Tween
+var current_display_value: int = 0
 
 func _on_game_score_changed(new_score: int) -> void:
-	%scoire.text = "Score: " + str(new_score)
+	if active_tween:
+		active_tween.kill()
+		
+	# 2. Create a new tween
+	active_tween = create_tween()
+	
+	# 3. Configure the tween to call our update method over a duration (e.g., 1.0 second)
+	active_tween.tween_method(
+		_update_label_text,      # The function to call every frame
+		current_display_value,   # Starting value
+		new_score,        # Target value received from the signal
+		0.2                     # Duration in seconds
+	)
+	
+func _update_label_text(value: int) -> void:
+	current_display_value = value
+	%scoire.text = "Score: " + str(value)  # Converts the interpolated integer into text
